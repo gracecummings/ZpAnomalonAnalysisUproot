241f222f2be4b2a8a3d08df58ebac30b2d429851
gracecummings <graceelainecummings@gmail.com>
2020-12-09 17:00:18 +0100

    fixed branch filling for four-vector candidates and individual values

--- TreeMakerTopiary.C
+++ TreeMakerTopiary.C
@@ -49,17 +49,25 @@ void TreeMakerTopiary::Loop(std::string outputFileName)
    double ZCandidate_phi;
    double ZCandidate_eta;
    double ZCandidate_m;
+   double hCandidate_pt;
+   double hCandidate_phi;
+   double hCandidate_eta;
+   double hCandidate_m;
    
    //Define the skimmed skim  output file and tree
    TFile* trimFile = new TFile(outputFileName.c_str(),"recreate");
    TTree* trimTree = fChain->CloneTree(0);
    TH1F*  hnskimed = new TH1F("hnskimed","first skimmed entires entries",1,0,1);
-   TBranch *hCand     = trimTree->Branch("hCandidate",&hCandidate,"hCandidate");
-   TBranch *ZCand     = trimTree->Branch("ZCandidate",&ZCandidate,"ZCandidate");
-   TBranch *ZCand_pt  = trimTree->Branch("ZCandidate_pt",&ZCandidate_pt,"ZCandidate_pt");
-   TBranch *ZCand_phi = trimTree->Branch("ZCandidate_phi",&ZCandidate_phi,"ZCandidate_phi");
-   TBranch *ZCand_eta = trimTree->Branch("ZCandidate_eta",&ZCandidate_eta,"ZCandidate_eta");
-   TBranch *ZCand_m   = trimTree->Branch("ZCandidate_m",&ZCandidate_eta,"ZCandidate_m");
+   TBranch *hCand     = trimTree->Branch("hCandidate","TLorentzVector",&hCandidate);
+   TBranch *hCand_pt  = trimTree->Branch("hCandidate_pt",&hCandidate_pt,"hCandidate_pt/D");
+   TBranch *hCand_phi = trimTree->Branch("hCandidate_phi",&hCandidate_phi,"hCandidate_phi/D");
+   TBranch *hCand_eta = trimTree->Branch("hCandidate_eta",&hCandidate_eta,"hCandidate_eta/D");
+   TBranch *hCand_m   = trimTree->Branch("hCandidate_m",&hCandidate_m,"hCandidate_m/D");
+   TBranch *ZCand     = trimTree->Branch("ZCandidate","TLorentzVector",&ZCandidate);
+   TBranch *ZCand_pt  = trimTree->Branch("ZCandidate_pt",&ZCandidate_pt,"ZCandidate_pt/D");
+   TBranch *ZCand_phi = trimTree->Branch("ZCandidate_phi",&ZCandidate_phi,"ZCandidate_phi/D");
+   TBranch *ZCand_eta = trimTree->Branch("ZCandidate_eta",&ZCandidate_eta,"ZCandidate_eta/D");
+   TBranch *ZCand_m   = trimTree->Branch("ZCandidate_m",&ZCandidate_m,"ZCandidate_m/D");
    hnskimed->SetBinContent(1,nentries);
 
 
@@ -87,32 +95,55 @@ void TreeMakerTopiary::Loop(std::string outputFileName)
       if (nZs > 0) {
 	std::vector<TLorentzVector>::iterator zit;
 	for (zit = ZCandidates->begin(); zit != ZCandidates->end(); ++zit) {
-	  double massZdiff = std::abs(91.18 - zit->M());
+	  double massZdiff = std::abs(91.1876 - zit->M());
 	  if ((massZdiff < baseZdiff) && (zit->M() >= 70.) && (zit->M() <= 110.)) {
 	    baseZdiff = massZdiff;
 	    theZ.SetPtEtaPhiM(zit->Pt(),zit->Phi(),zit->Eta(),zit->M());
-	    std::cout<<"The Zit  pt "<<zit->Pt()<<std::endl;
-	    std::cout<<"   The Z pt "<<theZ.Pt()<<std::endl;
+	    //std::cout<<"The Zit  pt "<<zit->Pt()<<std::endl;
+	    //std::cout<<"   The Z pt "<<theZ.Pt()<<std::endl;
 	    passZ = true;
 	  }
 	}
       }
+
+      //Higgs Candidates
+      unsigned int nfat = JetsAK8Clean->size();
+      TLorentzVector theh;
+      double basehdiff = 99999;
+      if (nfat > 0) {
+	std::vector<TLorentzVector>::iterator fit;
+	for (fit = JetsAK8Clean->begin(); fit != JetsAK8Clean->end(); ++fit) {
+	  double masshdiff = std::abs(125.18 - fit->M());
+	  if ((masshdiff < basehdiff) && (fit->Pt() > 250.)) {
+	    basehdiff = masshdiff;
+	    theh.SetPtEtaPhiM(fit->Pt(),fit->Phi(),fit->Eta(),fit->M());
+	    //std::cout<<"The fit  pt "<<fit->Pt()<<std::endl;
+	    //std::cout<<"   The h pt "<<theh.Pt()<<std::endl;
+	    passh = true;
+	  }
+	}
+      }
+      
 	
       if (passZ) {
-	//ZCandidate = theZ;
+	ZCandidate = theZ;
 	ZCandidate_pt  = theZ.Pt();
+	ZCand_pt->Fill();
 	ZCandidate_phi = theZ.Phi();
 	ZCandidate_eta = theZ.Eta();
 	ZCandidate_m   = theZ.M();
-	//ZCand->Fill();
-	ZCand_pt->Fill();
-	ZCand_phi->Fill();
-	ZCand_eta->Fill();
-	ZCand_m->Fill();
       }
-      
+
+      if (passh) {
+	hCandidate = theh;
+	hCandidate_pt  = theh.Pt();
+	//std::cout<<"   The h pt "<<hCandidate_pt<<std::endl;
+	hCandidate_phi = theh.Phi();
+	hCandidate_eta = theh.Eta();
+	hCandidate_m   = theh.M();
+      }
       //debug
-      if (jentry == 2000) {
+      if (jentry == 200) {
 	break;
       }
 

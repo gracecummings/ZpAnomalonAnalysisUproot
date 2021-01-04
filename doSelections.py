2fee9b6045d49129201cf868a25a7ed649b6f521
gracecummings <graceelainecummings@gmail.com>
2021-01-04 15:44:36 +0100

    fixed defaults for proper float naming for automatic cut sequence selection

--- doSelections.py
+++ doSelections.py
@@ -5,7 +5,7 @@ import numpy as np
 import argparse
 import glob
 import matplotlib.pyplot as plt
-import gecorg as go
+import gecorg_py3 as go
 
 parser = argparse.ArgumentParser()
 
@@ -14,12 +14,14 @@ if __name__=='__main__':
     parser.add_argument("-o","--output",help = "output file name")
     parser.add_argument("-b","--btagger",help = "btagger selection, need name part of key")
     parser.add_argument("-wp","--btagWP",type=float,default=0.6,help = "doulbeB tagger working point (default M1)")
-    parser.add_argument("-zpt","--zPtCut",type=float,default = 100,help = "pT cut on Z")
-    parser.add_argument("-hpt","--hPtCut",type=float,default = 250,help = "pT cut on h")
-    parser.add_argument("-met","--metPtCut",type=float,default = 50,help = "pT cut on met")
+    parser.add_argument("-zpt","--zPtCut",type=float,default = 100.0,help = "pT cut on Z")
+    parser.add_argument("-hpt","--hPtCut",type=float,default = 250.0,help = "pT cut on h")
+    parser.add_argument("-met","--metPtCut",type=float,default = 50.0,help = "pT cut on met")
+    parser.add_argument("-sdm","--sdmCut",type=float,default = 10.0,help = "lowest soft drop mass cut")
     args = parser.parse_args()
 
     samp   = args.sample
+    sdmcut = args.sdmCut
     zptcut = args.zPtCut
     hptcut = args.hPtCut
     metcut = args.metPtCut
@@ -42,7 +44,7 @@ if __name__=='__main__':
     ]
 
     #Example of plots
-    filename = go.makeOutFile(samp,'upout')
+    filename = go.makeOutFile(samp,'upout',str(zptcut),str(hptcut),str(metcut))
     outFile  = up3.recreate(filename,compression = None)
     
     #events = up3.iterate(inputfiles[:1],'PreSelection;1',branches=branches)
@@ -56,8 +58,8 @@ if __name__=='__main__':
         #print(b.keys())
         #print(b)
         #do some cuts
-        #sddf   = b[b['hCandidate_sd'] > 10.0]//moved to topiary
-        metdf  = b[b['METclean'] > metcut]
+        sddf   = b[b['hCandidate_sd'] > sdmcut]
+        metdf  = sddf[sddf['METclean'] > metcut]
         zptdf  = metdf[metdf['ZCandidate_pt'] > zptcut]
         hptdf  = zptdf[zptdf['hCandidate_pt'] > hptcut]
         lowsb  = hptdf[hptdf['hCandidate_sd'] <= 70.]

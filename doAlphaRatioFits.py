67770e6fdcc283c01a0c3783fc2b77de007d0a22
gracecummings <graceelainecummings@gmail.com>
2021-04-16 16:14:51 +0200

    all bkgs ready fro shape fits, improved plotting. Alpha ratio plots commented out, but there

--- doAlphaRatioFits.py
+++ doAlphaRatioFits.py
@@ -14,8 +14,20 @@ CMS_lumi.lumi_13TeV = "101.27 fb^{-1}"
 CMS_lumi.writeExtraText = 1
 CMS_lumi.extraText = "Simulation Preliminary"
 
-def plotMzp(pad,hist,histmax,textForPave):
+def setLogAxis(pad,islog):
+    if islog:
+        pad.SetLogy()
+
+def plotMzp(pad,hist,islog=False,logmin=0.1):
+    maxi = hist.GetMaximum()
+    mr   = round(maxi,0)
+    histmax = mr+mr*0.30
+    histmin = 0
+    if islog:
+        histmax = mr*100
+        histmin = logmin
     hist.SetMaximum(histmax)
+    hist.SetMinimum(histmin)
     hist.GetXaxis().SetTitle("M_{Z'}")
     hist.GetYaxis().SetTitle("Events / 45 GeV")
     hist.Draw()
@@ -24,23 +36,17 @@ if __name__=='__main__':
 
     #make the output
     #tc = ROOT.TCanvas("tc","alphafits",1100,400)
-    #p1 = ROOT.TPad("p1","sb",0,0,0.33,1.0)
-    #p2 = ROOT.TPad("p2","sr",0.33,0,0.66,1.0)
-    #p3 = ROOT.TPad("p3","alpha",0.66,0,1.0,1.0)
+    #p1 = ROOT.TPad("p1","datsb",0,0,0.5,1.0)
+    #p2 = ROOT.TPad("p2","alpha",0.5,0,1.0,1.0)
 
-    #tc2 = ROOT.TCanvas("tc2","ttvvshapes",1100,400)
-    #p21 = ROOT.TPad("p21","ttbar",0,0,0.33,1.0)
-    #p22 = ROOT.TPad("p22","vv",0.33,0,0.66,1.0)
-    #p23 = ROOT.TPad("p23","fittodata",0.66,0,1.0,1.0)
-
-    #make the output
-    tc = ROOT.TCanvas("tc","alphafits",1100,800)
-    p11 = ROOT.TPad("p11","ttbar",0,0,0.33,.5)
-    p12 = ROOT.TPad("p12","vv",0.33,0,0.66,.5)
-    p13 = ROOT.TPad("p13","fittodata",0.66,0,1.0,.5)
-    p21 = ROOT.TPad("p21","ttbar",0,.5,0.33,1.0)
-    p22 = ROOT.TPad("p22","vv",0.33,.5,0.66,1.0)
-    p23 = ROOT.TPad("p23","fittodata",0.66,.5,1.0,1.0)
+    tc = ROOT.TCanvas("tc","shapes",1100,800)
+    p11 = ROOT.TPad("p11","dysr",0,0,0.33,.5)
+    p12 = ROOT.TPad("p12","ttsr",0.33,0,0.66,.5)
+    p13 = ROOT.TPad("p13","vvsr",0.66,0,1.0,.5)
+    p21 = ROOT.TPad("p21","dysb",0,.5,0.33,1.0)
+    p22 = ROOT.TPad("p22","ttsb",0.33,.5,0.66,1.0)
+    p23 = ROOT.TPad("p23","vvsb",0.66,.5,1.0,1.0)
+        
     
     #will replace with command line options
     bkg_dir = 'analysis_output_ZpAnomalon/2021-04-16/'
@@ -58,13 +64,20 @@ if __name__=='__main__':
     empty3 = empty.Clone()
     empty4 = empty.Clone()
     empty5 = empty.Clone()
+    empty6 = empty.Clone()
+    empty7 = empty.Clone()
+    empty8 = empty.Clone()
     
     hsbdy = bkgs.getAddedHist(empty,"DYJetsToLL","sb","h_zp_jigm")
     hsrdy = bkgs.getAddedHist(empty2,"DYJetsToLL","sr","h_zp_jigm")
     hsbtt = bkgs.getAddedHist(empty3,"TT","sb","h_zp_jigm")
+    hsrtt = bkgs.getAddedHist(empty6,"TT","sr","h_zp_jigm")
     hsbzz = bkgs.getAddedHist(empty4,"ZZTo2L2Q","sb","h_zp_jigm")
+    hsrzz = bkgs.getAddedHist(empty7,"ZZTo2L2Q","sr","h_zp_jigm")
     hsbwz = bkgs.getAddedHist(empty5,"WZTo2L2Q","sb","h_zp_jigm")
+    hsrwz = bkgs.getAddedHist(empty8,"WZTo2L2Q","sr","h_zp_jigm")
     hsbvv = hsbzz.Add(hsbwz)
+    hsrvv = hsrzz.Add(hsrwz)
 
     ROOT.gSystem.CompileMacro("cfunctions/alphafits.C","kfc")
     ROOT.gSystem.Load("cfunctions/alphafits_C")
@@ -73,13 +86,14 @@ if __name__=='__main__':
     histmax = 15.
     ROOT.gStyle.SetOptFit(0)
     ROOT.gStyle.SetOptStat(0)
+    islog = True
 
     tc.cd()
-    p11.Draw()
-    p11.cd()
-    plotMzp(p11,hsbdy,histmax,'sideband')
-    CMS_lumi.CMS_lumi(p11,4,13)
-    p11.Update()
+    p21.Draw()
+    p21.cd()
+    plotMzp(p21,hsbdy)
+    CMS_lumi.CMS_lumi(p21,4,13)
+    p21.Update()
     sbfit = ROOT.expFit(hsbdy,"sbl","QR0+")
     sbfit.Draw("SAME")
     label = ROOT.TPaveText(.5,.5,.9,.7,"NBNDC")
@@ -88,16 +102,16 @@ if __name__=='__main__':
     label.AddText("150 < m_{hcand,SD}")
     label.SetFillColor(0)
     label.Draw()
-    p11.Update()
+    p21.Update()
      
     tc.cd()
-    p12.Draw()
-    p12.cd()
-    plotMzp(p12,hsrdy,6,"foo")
+    p11.Draw()
+    p11.cd()
+    plotMzp(p11,hsrdy)
     srfit = ROOT.expFit(hsrdy,"srl","QR0+")
     srfit.Draw("SAME")
-    CMS_lumi.CMS_lumi(p12,4,13)
-    p12.Update()
+    CMS_lumi.CMS_lumi(p11,4,13)
+    p11.Update()
 
     label2 = ROOT.TPaveText(.5,.5,.9,.7,"NBNDC")
     label2.AddText("DY MC SR")
@@ -105,29 +119,49 @@ if __name__=='__main__':
     label2.AddText("70 < m_{hcand,SD} < 110")
     label2.SetFillColor(0)
     label2.Draw()
-    p12.Update()
+    p11.Update()
     
-    tc.cd()
-    p13.Draw()
-    p13.cd()
-    alpha = ROOT.alphaRatioMakerExp(hsbdy,hsrdy)
-    alpha.GetYaxis().SetTitle("alpha(M_{Z'})")
-    alpha.GetXaxis().SetTitle("M_{Z'}")
-    alpha.Draw()
-
-    tc.cd()
-    p21.Draw()
-    p21.cd()
-    plotMzp(p21,hsbtt,5,"fff")
-    CMS_lumi.CMS_lumi(p21,4,13)
-    p21.Update()
+    #tc.cd()
+    #p22.Draw()
+    #p22.cd()
+    #alpha = ROOT.alphaRatioMakerExp(hsbdy,hsrdy)
+    #alpha.GetYaxis().SetTitle("alpha(M_{Z'})")
+    #alpha.GetXaxis().SetTitle("M_{Z'}")
+    #alpha.Draw()
 
     tc.cd()
     p22.Draw()
     p22.cd()
-    plotMzp(p22,hsbzz,5,"fff")
+    setLogAxis(p22,islog)
+    plotMzp(p22,hsbtt,islog)
     CMS_lumi.CMS_lumi(p22,4,13)
     p22.Update()
+
+    tc.cd()
+    p12.Draw()
+    p12.cd()
+    setLogAxis(p12,islog)
+    plotMzp(p12,hsrtt,islog)
+    CMS_lumi.CMS_lumi(p12,4,13)
+    p12.Update()
+
+    tc.cd()
+    p23.Draw()
+    p23.cd()
+    setLogAxis(p23,islog)
+    plotMzp(p23,hsbzz,islog,0.001)
+    CMS_lumi.CMS_lumi(p23,4,13)
+    p23.Update()
+
+    tc.cd()
+    p13.Draw()
+    p13.cd()
+    setLogAxis(p13,islog)
+    plotMzp(p13,hsrzz,islog,0.001)
+    CMS_lumi.CMS_lumi(p13,4,13)
+    p13.Update()
+
+    
     figalpha = go.makeOutFile('Run2_2017_2018','alpha_fits','.png',str(zptcut),str(hptcut),str(metcut),str(btagwp))
     #figshapes = go.makeOutFile('Run2_2017_2018','alpha_shapes','.png',str(zptcut),str(hptcut),str(metcut),str(btagwp))
     tc.SaveAs(figalpha)
